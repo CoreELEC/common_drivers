661fd769ebc9f4419f910df2a5d39d0a8ce16e98
Portisch <hugo.portisch@yahoo.de>
2024-02-06 13:10:52 +0100

    mbox: add support of u-boot 2015

--- drivers/mailbox/meson_mbox_comm.h
+++ drivers/mailbox/meson_mbox_comm.h
@@ -13,8 +13,21 @@ struct mbox_cmd_t {
 	u32 sync:7;
 };
 
+struct mbox_cmd_old_t {
+	u32 cmd:8;
+	u32 client_id:8;
+	u32 res1:4;
+	u32 size:9;
+	u32 res2:3;
+};
+
 union mbox_stat {
 	u32 set_cmd;
 	struct mbox_cmd_t  mbox_cmd_t;
+};
+
+union mbox_stat_old {
+	u32 set_cmd;
+	struct mbox_cmd_old_t  mbox_cmd_t;
 };
 #endif
