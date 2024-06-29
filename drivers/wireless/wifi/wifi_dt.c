8d215fdf393b9cabcc2afb8e3f2ff89fa2bfa5aa
Peter <peter.vicman@gmail.com>
2024-06-29 22:01:29 +0200

    pcie: remove AP6276P Wifi 6 PCIe device and then also bus
    [    4.196920@2]  wifi_dummy: loading out-of-tree module taints kernel.
    [    4.197218@2]  Triggered SDIO/PCI WiFi power on and bus rescan.
    [    4.197231@2]  aml_wifi aml_wifi: [pci_remove] pci remove!
    [    4.197242@2]  aml_wifi aml_wifi: [pci_remove] found device 0x14e4:0x44a0, remove it!
    [    4.197414@2]  aml_wifi aml_wifi: [pci_remove] remove bus!
    [    4.197574@2]  pci_bus 0000:01: busn_res: [bus 01-ff] is released
    [    4.402580@2]  aml_wifi aml_wifi: [usb_power_control] Set WiFi power down
    [    4.402607@2]  aml_wifi aml_wifi: [extern_wifi_set_enable] WIFI  Disable! 436
    [    4.734571@2]  aml_wifi aml_wifi: [extern_wifi_set_enable] WIFI  Enable! 436
    [    4.734585@2]  aml_wifi aml_wifi: [usb_power_control] Set WiFi power on !
    [    4.942582@2]  Error: sdio_host is NULL
    [    4.942597@2]  [sdio_reinit] finish
    [    4.942607@2]  aml_wifi aml_wifi: [usb_power_control] Set WiFi power on !
    [    5.278566@1]  Error: sdio_host is NULL
    [    5.278578@1]  [sdio_reinit] finish
    [    5.278586@1]  aml_wifi aml_wifi: [pci_reinit] pci wifi reinit!
    
    fixes ap6xxx-aml + wifi_dummy-aml = Kernel panic
    
    [   30.580751@4]  Kernel panic - not syncing: Asynchronous SError Interrupt
    [   30.580752@4]  CPU: 4 PID: 1117 Comm: modprobe Tainted: G S         O      5.4.210 #1
    [   30.580753@4]  Hardware name: Ugoos AM8 (DT)
    [   30.580754@4]  Call trace:
    [   30.580755@4]   dump_backtrace+0x0/0x180
    [   30.580756@4]   show_stack+0x2c/0x3c
    [   30.580758@4]   dump_stack+0xbc/0xe8
    [   30.580759@4]   panic+0x150/0x338
    [   30.580760@4]   nmi_panic+0x98/0xa0
    [   30.580761@4]   arm64_serror_panic+0x90/0x9c
    [   30.580762@4]   arm64_is_fatal_ras_serror+0x48/0x100
    [   30.580763@4]   do_serror+0xf8/0x184
    [   30.580765@4]   el1_error+0xbc/0x160
    [   30.580766@4]   pstore_io_save+0x23c/0x380
    [   30.580767@4]   dhdpcie_dongle_attach+0x4c/0x1540 [dhdpci]
    [   30.580769@4]   dhdpcie_bus_attach+0xac/0x5b0 [dhdpci]
    [   30.580770@4]   dhdpcie_pci_probe+0x6f8/0x1030 [dhdpci]
    [   30.580771@4]   pci_device_probe+0xc4/0x160
    [   30.580772@4]   really_probe+0x104/0x514
    [   30.580774@4]   driver_probe_device+0xf4/0x140
    [   30.580775@4]   device_driver_attach+0xcc/0xd4
    [   30.580776@4]   __driver_attach+0xb0/0x16c
    [   30.580777@4]   bus_for_each_dev+0x74/0xb0
    [   30.580778@4]   driver_attach+0x38/0x50
    [   30.580779@4]   bus_add_driver+0x15c/0x260
    [   30.580780@4]   driver_register+0x80/0x134
    [   30.580782@4]   __pci_register_driver+0x60/0x74
    [   30.580783@4]   dhdpcie_bus_register+0x40/0x1a0 [dhdpci]
    [   30.580784@4]   dhd_bus_register+0x28/0x60 [dhdpci]
    [   30.580785@4]   dhd_wifi_platform_load+0x64/0x4b8 [dhdpci]
    [   30.580786@4]   dhd_wifi_platform_register_drv+0x1f4/0x240 [dhdpci]
    [   30.580788@4]   dhd_module_init+0x134/0x
    [   30.580789@4]  Lost 8 message(s)!
    [   30.580883@4]  SMP: stopping secondary CPUs
    [   30.580885@4]  Kernel Offset: disabled
    [   30.580886@4]  CPU features: 0x00030006,2a00aa10
    [   30.580887@4]  Memory Limit: none

--- drivers/wireless/wifi/wifi_dt.c
+++ drivers/wireless/wifi/wifi_dt.c
@@ -44,7 +44,8 @@ struct pcie_wifi_chip {
 
 static const struct pcie_wifi_chip pcie_wifi[] = {
 	{0x16c3, 0xabcd},
-	{0x1556, 0x1111}
+	{0x1556, 0x1111},
+	{0x14e4, 0x44a0}  /* AP6276P Wifi 6 PCIe module */
 };
 
 int wifi_power_gpio;
