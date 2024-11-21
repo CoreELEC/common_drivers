af2014fc2a1f077785cbf3108189efea8bdf0a28
Portisch <hugo.portisch@yahoo.de>
2024-11-21 08:14:10 +0100

    fix: amvecm: remove unnecessary matrix settings matrix should not be enabled above s5 as it will show strange colours on using fbdev.

--- drivers/media/enhancement/amvecm/set_hdr2_v0.c
+++ drivers/media/enhancement/amvecm/set_hdr2_v0.c
@@ -3180,7 +3180,8 @@ enum hdr_process_sel hdr_func(enum hdr_module_sel module_sel,
 		/* turn off OSD mtx and use HDR for g12, sm1, tl1 */
 		if (!osd_pic_en ||
 			chip_type_id != chip_t5m) {
-			/* VSYNC_WRITE_VPP_REG_VPP_SEL(VPP_WRAP_OSD1_MATRIX_EN_CTRL, 0, vpp_sel); */
+			if (chip_type_id < chip_t3x)
+				VSYNC_WRITE_VPP_REG_VPP_SEL(VPP_WRAP_OSD1_MATRIX_EN_CTRL, 0, vpp_sel);
 			if (!is_amdv_on())
 				hdr_process_select |= RGB_OSD;
 		}
