a4d57ec44d09aa6582f4aee853b320b8beea121e
Marek Czerski <m.czerski@ap-tech.pl>
2025-09-22 17:23:34 +0200

    AML: DVB: fix for SC2 DVB not receiving stream from demod
    background:
    Demux driver does not work on SC2 device that was originally
    released with kernel v4.9 (mecool KT1)
    The issue was introduced in kernel v5.4 with:
    
    commit 15954fc01f8a97f897d6daf21699a11f466e8d1a
    Author: chuangcheng peng <chuangcheng.peng@amlogic.com>
    Date:   Thu Mar 3 19:16:01 2022 +0800
    
        demux: use new tee interface to set [1/1]
    
        PD#SWPL-74508
    
        Problem:
        tee flag conflict
    
        Solution:
        call new tee interface
    
        Verify:
        not verified
    
        Change-Id: Iccd8988af3a2ec612d858951dfc871d97bb4f1ac
        Signed-off-by: chuangcheng peng <chuangcheng.peng@amlogic.com>
    
    action taken:
    Introduce new dts parameter tee_compat. If set, the old behaviour of the
    driver is enabled which solves the issue (for mecool KT1).

--- drivers/dvb/demux/aml_dvb.c
+++ drivers/dvb/demux/aml_dvb.c
@@ -70,10 +70,15 @@ static int force_tsn_source;
 
 int is_security_dmx;
 
-static void demux_config_pipeline(int cfg_demod_tsn, int cfg_tsn_out)
+static void demux_config_pipeline(int cfg_demod_tsn, int cfg_tsn_out, int tee_compat)
 {
 	u32 value = 0;
 	int ret = 0;
+
+	if (tee_compat) {
+		tee_demux_config_pipeline(cfg_demod_tsn, cfg_tsn_out);
+		return;
+	}
 
 	value = cfg_demod_tsn;
 	value += cfg_tsn_out << 1;
@@ -332,7 +337,7 @@ ssize_t tsn_source_store(struct class *class,
 //	pr_dbg("tsn_in:%d, tsn_out:%d\n", tsn_in_reg, tsn_out);
 	advb->dsc_pipeline = tsn_in_reg;
 	//set demod/local
-	demux_config_pipeline(tsn_in_reg, tsn_out);
+	demux_config_pipeline(tsn_in_reg, tsn_out, advb->tee_compat);
 
 	mutex_unlock(&advb->mutex);
 	return count;
@@ -341,6 +346,7 @@ ssize_t tsn_source_store(struct class *class,
 int tsn_set_double_out(int flag)
 {
 	int tsn_in_reg = 0;
+	struct aml_dvb *advb = aml_get_dvb_device();
 
 	if (tsn_out == flag)
 		return 0;
@@ -350,7 +356,7 @@ int tsn_set_double_out(int flag)
 	if (tsn_in == INPUT_DEMOD)
 		tsn_in_reg = 1;
 
-	demux_config_pipeline(tsn_in_reg, tsn_out);
+	demux_config_pipeline(tsn_in_reg, tsn_out, advb->tee_compat);
 	return 0;
 }
 
@@ -369,7 +375,7 @@ int tsn_source_force_set(int source)
 	tsn_in = source;
 	advb->dsc_pipeline = tsn_in_reg;
 
-	demux_config_pipeline(tsn_in_reg, 0);
+	demux_config_pipeline(tsn_in_reg, 0, advb->tee_compat);
 	force_tsn_source = 1;
 	return 0;
 }
@@ -678,6 +684,21 @@ int dmx_get_tsn_flag(struct platform_device *pdev, int *tsn_in, int *tsn_out)
 	return 0;
 }
 
+u32 dmx_get_tee_compat_flag(struct platform_device *pdev)
+{
+	char buf[32];
+	u32 tee_compat = 0;
+	int ret = 0;
+
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, sizeof(buf), "tee_compat");
+	ret = of_property_read_u32(pdev->dev.of_node, buf, &tee_compat);
+	if (!ret)
+		dprint_i("%s:%d\n", buf, tee_compat);
+
+	return tee_compat;
+}
+
 struct aml_dvb *aml_get_dvb_device(void)
 {
 	return &aml_dvb_device;
@@ -843,8 +864,9 @@ static int aml_dvb_probe(struct platform_device *pdev)
 
 	pr_dbg("tsn_in:%d, tsn_out:%d\n", tsn_in_reg, tsn_out);
 	advb->dsc_pipeline = tsn_in_reg;
+	advb->tee_compat = dmx_get_tee_compat_flag(pdev);
 	//set demod/local
-	demux_config_pipeline(tsn_in_reg, tsn_out);
+	demux_config_pipeline(tsn_in_reg, tsn_out, advb->tee_compat);
 
 	dmx_init_hw();
 
